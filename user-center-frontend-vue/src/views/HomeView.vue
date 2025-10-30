be0a788fef23e97f6a12a4abd6d0c37be8db41cd
ifsihj <2334247405@qq.com>
2025-10-30 11:35:19 +0800

    iframe test

--- user-center-frontend-vue/src/views/HomeView.vue
+++ user-center-frontend-vue/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <a-button type="primary" danger>Primary</a-button>
-    <img alt="Vue logo" src="../assets/logo.svg" />
+    <!-- <img alt="Vue logo" src="../assets/logo.svg" /> -->
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
--- user-center-frontend-vue/src/views/projectManagement.vue
+++ user-center-frontend-vue/src/views/projectManagement.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="projectManagement">This is the Project Management page.</div>
+  <div class="projectManagement">
+    <div class="iframe-wrapper">
+      <iframe src="https://www.pku.edu.cn/" width="1200" height="900"></iframe>
+    </div>
+  </div>
 </template>
 <script setup lang="ts"></script>
-<style scoped></style>
+<style scoped>
+.iframe-wrapper {
+  width: 100%;
+  height: 570px;
+  overflow: hidden;
+  position: relative;
+}
+.iframe-wrapper iframe {
+  transform: scale(0.9); /* 缩小到 80% */
+  transform-origin: top left;
+  width: 1200px;
+  height: 800px;
+  border: none;
+}
+</style>
